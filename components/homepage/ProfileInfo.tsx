644baccb996469b3e7c752e71013d315a231fa22
N S RAWAT <99235597+iNSRawat@users.noreply.github.com>
2025-11-10 10:39:26 +0530

    Update ProfileInfo.tsx

--- components/homepage/ProfileInfo.tsx
+++ components/homepage/ProfileInfo.tsx
@@ -6,15 +6,15 @@ import { Briefcase, MapPin, Mail, Linkedin, Github, Facebook } from 'lucide-reac
 const ProfileCardInfo = () => {
   return (
     <div className="p-3">
-      <h3 className="text-lg font-medium text-gray-800 dark:text-white">Trong Khanh (Karhdo) Do</h3>
+      <h3 className="text-lg font-medium text-gray-800 dark:text-white">Nagendra Rawat (N S Rawat) Do</h3>
       <h5 className="text-gray-700 dark:text-gray-400">Learner | Builder</h5>
       <div className="mb-2 mt-2 space-y-3">
         <div className="flex items-center">
           <Briefcase size={20} strokeWidth={1} />
           <p className="px-2">
-            Fullstack Engineer @{' '}
+            Data Scientist & ML Enthusiast @{' '}
             <a className="hover:underline" target="_blank" href="https://younetmedia.com/" rel="noreferrer">
-              YouNet Media
+              Student/Learner 
             </a>
           </p>
         </div>
