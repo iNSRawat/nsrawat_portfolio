fae7a07c951fbc789e2a577cf6197ccb1988419b
N S RAWAT <99235597+iNSRawat@users.noreply.github.com>
2025-11-24 15:25:47 +0530

    Fix Prettier errors in GrowingUnderline component

--- components/ui/GrowingUnderline.tsx
+++ components/ui/GrowingUnderline.tsx
@@ -1,5 +1,5 @@
-import { clsx } from 'clsx'
-import type { CSSProperties } from 'react'
+import { clsx } from 'clsx';
+import type { CSSProperties } from 'react';
 
 export function GrowingUnderline({
   as: Component = 'span',
@@ -9,22 +9,20 @@ export function GrowingUnderline({
   duration,
   ...rest
 }: {
-  children: React.ReactNode
-  as?: React.ElementType
-  active?: boolean
-  className?: string
-  duration?: number
+  children: React.ReactNode;
+  as?: React.ElementType;
+  active?: boolean;
+  className?: string;
+  duration?: number;
 } & React.ComponentProps<'span'>) {
   return (
     <Component
       className={clsx([
         'bg-linear-to-r bg-bottom-left bg-no-repeat',
-        'transition-[background-size] duration-(--duration,300ms)',
+        'duration-(--duration,300ms) transition-[background-size]',
         'from-green-200 to-green-100',
         'dark:from-emerald-800 dark:to-emerald-900',
-        active
-          ? 'bg-size-[100%_50%] hover:bg-size-[100%_100%]'
-          : 'bg-size-[0px_50%] hover:bg-size-[100%_50%]',
+        active ? 'bg-size-[100%_50%] hover:bg-size-[100%_100%]' : 'bg-size-[0px_50%] hover:bg-size-[100%_50%]',
         className,
       ])}
       style={{ '--duration': `${duration || 300}ms` } as CSSProperties}
@@ -32,5 +30,5 @@ export function GrowingUnderline({
     >
       {children}
     </Component>
-  )
+  );
 }
