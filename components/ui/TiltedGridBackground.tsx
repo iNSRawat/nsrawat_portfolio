944964846370a8b8113905729eb11a02e941367b
N S RAWAT <99235597+iNSRawat@users.noreply.github.com>
2025-11-30 12:03:23 +0530

    Refactor TiltedGridBackground to remove SVG import issue

--- components/ui/TiltedGridBackground.tsx
+++ components/ui/TiltedGridBackground.tsx
@@ -1,10 +1,8 @@
 import { clsx } from 'clsx';
 
-// import TiltedGrid from 'public/static/icons/tilted-grid.svg';
-
 const TiltedGridBackground = ({ className }: { className?: string }) => {
   return (
-    <div className={clsx(['absolute overflow-hidden [mask-image:linear-gradient(white,transparent)]', className])}>
+    <div className={clsx(['absolute overflow-hidden [mask-image:linear-gradient(white,transparent)]', className])} />
   );
 };
 
