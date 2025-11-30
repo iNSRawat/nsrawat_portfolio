755e8982a9ca1c79670bc593eeac65a74f1f3b9a
N S RAWAT <99235597+iNSRawat@users.noreply.github.com>
2025-11-30 11:48:18 +0530

    Remove unused BrandIcons: Keep only Data Science & ML related icons

--- components/ui/BrandIcon.tsx
+++ components/ui/BrandIcon.tsx
@@ -1,22 +1,5 @@
-import Bash from 'public/static/icons/bash.svg';
-import Git from 'public/static/icons/git.svg';
-import GitHub from 'public/static/icons/github.svg';
 import Javascript from 'public/static/icons/javascript.svg';
-import Markdown from 'public/static/icons/markdown.svg';
-import NextJS from 'public/static/icons/nextjs.svg';
-import Node from 'public/static/icons/nodejs.svg';
-import Railway from 'public/static/icons/railway.svg';
-import React from 'public/static/icons/react.svg';
-import Remix from 'public/static/icons/remix.svg';
-import Spotify from 'public/static/icons/spotify.svg';
-import TailwindCSS from 'public/static/icons/tailwind.svg';
 import Typescript from 'public/static/icons/typescript.svg';
-import Umami from 'public/static/icons/umami.svg';
-import Vercel from 'public/static/icons/vercel.svg';
-import NestJS from 'public/static/icons/nestjs.svg';
-import Docker from 'public/static/icons/docker.svg';
-import Postgres from 'public/static/icons/postgres.svg';
-import Mongodb from 'public/static/icons/mongodb.svg';
 import Python from 'public/static/icons/python.svg';
 import SQL from 'public/static/icons/sql.svg';
 import Matplotlib from 'public/static/icons/matplotlib.svg';
@@ -24,35 +7,23 @@ import NumPy from 'public/static/icons/numpy.svg';
 import Pandas from 'public/static/icons/pandas.svg';
 import Seaborn from 'public/static/icons/seaborn.svg';
 import Plotly from 'public/static/icons/plotly.svg';
-import ScikitLearn from 'public/static/icons/scikit-learn.svg';
+import Scikit-learn from 'public/static/icons/scikit-learn.svg';
 import TensorFlow from 'public/static/icons/tensorflow.svg';
 import PyTorch from 'public/static/icons/pytorch.svg';
 import PowerBI from 'public/static/icons/powerbi.svg';
-import Jupyter from 'public/static/icons/jupyter.svg';
 import Tableau from 'public/static/icons/tableau.svg';
+import Jupyter from 'public/static/icons/jupyter.svg';
 import Anaconda from 'public/static/icons/anaconda.svg';
 import PyCharm from 'public/static/icons/pycharm.svg';
+import Mongodb from 'public/static/icons/mongodb.svg';
 
-export const BrandIconsMap = {
-  React,
-  Remix,
-  Git,
-  GitHub,
+type BrandIconsMap = {
+  [key: string]: React.ElementType;
+};
+
+const BrandIconsMap: BrandIconsMap = {
   Javascript,
   Typescript,
-  Node,
-  Bash,
-  Markdown,
-  NextJS,
-  TailwindCSS,
-  Umami,
-  Vercel,
-  Railway,
-  Spotify,
-  NestJS,
-  Docker,
-  Postgres,
-  Mongodb,
   Python,
   SQL,
   Matplotlib,
@@ -60,19 +31,20 @@ export const BrandIconsMap = {
   Pandas,
   Seaborn,
   Plotly,
-  ScikitLearn,
+  "Scikit-learn": Scikit-learn,
   TensorFlow,
   PyTorch,
   PowerBI,
-  Jupyter,
   Tableau,
+  Jupyter,
   Anaconda,
   PyCharm,
+  Mongodb,
 };
 
 export type BrandIconType = keyof typeof BrandIconsMap;
 
-const BrandIcon = (props: { type: keyof typeof BrandIconsMap; className?: string }) => {
+const BrandIcon = (props: { type: BrandIconType; className?: string; classNameSvg?: string }) => {
   const { type, className } = props;
 
   const Icon = BrandIconsMap[type];
